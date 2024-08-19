d585100751c2da66d2556e8f928403206f23a0f7
Bastian-Bouffanais <105658626+Bastian-Bouffanais@users.noreply.github.com>
2024-08-19 02:59:39 -0400

    pagina versión 2

---
+++ assets/js/script_2.js
@@ -0,0 +1,36 @@
+var typed_name = new Typed('#name', {
+    strings: ['Bastián Bouffanais'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_rol = new Typed('#rol', {
+    strings: ['Estudiante'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_email = new Typed('#email', {
+    strings: ['bastibouffanais@gmail.com'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_linkedin = new Typed('#linkedin', {
+    strings: ['Bastián Bouffanais'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_github = new Typed('#github', {
+    strings: ['Bastian-Bouffanais'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+
+var typed_desarrollo = new Typed('#desarrollo', {
+    strings: ['Desarrollador Full-Stack'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
+var typed_opensource = new Typed('#opensource', {
+    strings: ['Proyectos Open Source'],
+    typeSpeed: 20,
+    showCursor: false,
+ });
